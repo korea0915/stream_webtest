2b989beade29e6631afe3d372963ee5bbdf4b0b5
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 17:09:21 +0900

    vv2

--- opendata/app.py
+++ opendata/app.py
@@ -4,8 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "지역 선택",
+    ("서울", "인천", "등등")
 )
 
 # Using "with" notation
@@ -14,3 +14,17 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
