54241fe4a391adbe554a812ee381788c9e5869ab
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 14:38:30 +0900

    16123

--- lucky/app.py
+++ lucky/app.py
@@ -55,11 +55,13 @@ ss2 = ss[ss.ne("")]
 # str: string 관련된 메소드를 사용할 수 있게 함
 n_idx = ss2.index.str.contains('n')
 n_data = ss2[n_idx]
-st.write(n_data)
 
 g_idx = ss2.index.str.contains('g')
 g_data = ss2[g_idx]
-st.write(g_data)
-
 # <추첨 버튼>
 # 13개의 짝을 지어서 표시해줄 그래픽
+
+n_rd = np.random.choice(n_data, len(n_data), replace=False)
+st.write(n_rd)
+g_rd = np.random.choice(n_data, len(g_data), replace=False)
+st.write(g_rd)
