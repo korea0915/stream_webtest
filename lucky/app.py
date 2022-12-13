51a1ab8a4764e2ac4f2055a9dd99f709ff98d1b8
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 12:40:16 +0900

    dd

--- lucky/app.py
+++ lucky/app.py
@@ -5,5 +5,5 @@ st.title("조 추첨 페이지")
 st.header("여러분의 참여를 환영합니다.")
 
 colums = st.colums(4)
-for c in colums:
-    st.text_input("조 추첨 대상")
+for idx, c in colums:
+    st.text_input("조 추첨 대상" key=idx)
