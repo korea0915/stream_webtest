d44a7b18054ca541accd115ea830d22382fceded
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 20:34:50 +0900

    zxc

--- opendata/app.py
+++ opendata/app.py
@@ -17,7 +17,7 @@ with st.sidebar:
     )
 
 if st.sidebar.button('지역 선택'):
-    location = st.sidebar.selectbox(
+    location = with st.sidebar.selectbox(
     "지역 선택",
         (
             '강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
@@ -25,11 +25,10 @@ if st.sidebar.button('지역 선택'):
             '중랑구', '동대문구', '광진구', '성동구', '용산구', '중구', '종로구'
         )
     )
-    st.sidebar.button('지역 선택')
     st.write(location)
 
 if st.sidebar.button('크기 선택'):
-    size = st.sidebar.selectbox(
+    size = with st.sidebar.selectbox(
     "크기 선택",
         (
             '소형', '중소형', '중형', '중대형', '대형'
