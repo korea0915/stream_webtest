47aa0a88922170737c2dd8825513009b6b6209a8
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 11:44:44 +0900

    streamlit메인 작성

---
+++ first_webapp/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write(
+    """
+    # stramlit기반의 웹 페이지연습
+    ## 마크다운으로 작성
+    * 테스트 클립니다.
+    """
+)
+
+st.image(
+    "https://imagescdn.gettyimagesbank.com/500/201607/a10518606.jpg"
+)
