91d085d679d1e19dc3308df5e71d45eb0a349adb
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 20:19:00 +0900

    test

--- opendata/app.py
+++ opendata/app.py
@@ -15,17 +15,16 @@ with st.sidebar:
         "원하시는 년도를 선택해 주세요",
         (AI26year[0], AI26year[1], AI26year[2])
     )
- s = st.sidebar.button('지역 선택')
-    if s:
-        location = st.sidebar.selectbox(
-        "지역 선택",
-            (
-                '강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
-                '강서구', '양천구', '마포구', '서대문구', '은평구', '노원구', '도봉구', '강북구', '성북구',
-                '중랑구', '동대문구', '광진구', '성동구', '용산구', '중구', '종로구'
-            )
-        )
-        temp1 = location
+st.sidebar.button('지역 선택', on_click (showdata())):
+    # location = st.sidebar.selectbox(
+    # "지역 선택",
+    #     (
+    #         '강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
+    #         '강서구', '양천구', '마포구', '서대문구', '은평구', '노원구', '도봉구', '강북구', '성북구',
+    #         '중랑구', '동대문구', '광진구', '성동구', '용산구', '중구', '종로구'
+    #     )
+    # )
+    # temp1 = location
 st.write(temp1)
 if st.sidebar.button('크기 선택'):
     size = st.sidebar.selectbox(
@@ -53,3 +52,14 @@ with st.expander("결론"):
         be random.
     """)
     st.image("https://static.streamlit.io/examples/dice.jpg")
+
+
+def showdata():
+    location = st.sidebar.selectbox(
+    "지역 선택",
+        (
+            '강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
+            '강서구', '양천구', '마포구', '서대문구', '은평구', '노원구', '도봉구', '강북구', '성북구',
+            '중랑구', '동대문구', '광진구', '성동구', '용산구', '중구', '종로구'
+        )
+    )
