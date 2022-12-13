1ab08e5f4a35944c55d2d21d956b2c388985462a
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 14:36:11 +0900

    v1212

--- lucky/app.py
+++ lucky/app.py
@@ -56,7 +56,8 @@ ss2 = ss[ss.ne("")]
 n_idx = ss2.index.str.contains('n')
 n_data = ss2[n_idx]
 st.write(n_data)
-g_idx = ss2.index.str.contains('n')
+
+g_idx = ss2.index.str.contains('g')
 g_data = ss2[g_idx]
 st.write(g_data)
 
