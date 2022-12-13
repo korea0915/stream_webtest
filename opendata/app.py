4ce75ba69bd96cdca339211a71fd4cc7ca70a07d
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 17:06:38 +0900

    sdf

--- opendata/app.py
+++ opendata/app.py
@@ -2,3 +2,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
