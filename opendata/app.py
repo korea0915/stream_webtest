a4aaab552b926c76fb60c402891bc0228a300c96
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 17:10:13 +0900

    vv123

--- opendata/app.py
+++ opendata/app.py
@@ -15,16 +15,11 @@ with st.sidebar:
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+data = np.random.randn(10, 1)
 
-with tab1:
-   st.header("A cat")
-   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+tab1.subheader("A tab with a chart")
+tab1.line_chart(data)
 
-with tab2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+tab2.subheader("A tab with the data")
+tab2.write(data)
