84cf935ccec8a0d6eb5c181bd509b985207e47d8
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 14:15:07 +0900

    v2

--- lucky/app.py
+++ lucky/app.py
@@ -3,8 +3,12 @@ import numpy as np
 
 st.title("조 추첨 페이지")
 st.header("여러분의 참여를 환영합니다.")
-colums = st.colums(4)
+colums = st.columns(4)
 
 for idx,col in enumerate(colums):
     for idx2 in range(4):
-        col.text_input(f"조 추첨 대상 {idx+1 +idx2*4}", key=f"{idx+1+idx2*4}")
+        col.text_input
+        (
+            f"조 추첨 대상 {idx+1 +idx2*4}", 
+            key=f"{idx+1+idx2*4}"
+        )
