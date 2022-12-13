883b31e5afe58cb7b739a6e247c522901e1eea16
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 12:41:26 +0900

    test

--- lucky/app.py
+++ lucky/app.py
@@ -4,6 +4,6 @@ import numpy as np
 st.title("조 추첨 페이지")
 st.header("여러분의 참여를 환영합니다.")
 
-colums = st.colums(4)
-for idx, c in colums:
-    st.text_input("조 추첨 대상" key=idx)
+# colums = st.colums(4)
+# for idx, c in colums:
+#     st.text_input("조 추첨 대상" key=idx)
