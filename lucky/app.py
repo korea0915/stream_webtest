fa8e42d2a6c4b59ddb9251843bc4058504d3b417
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 13:57:33 +0900

    dsa

--- lucky/app.py
+++ lucky/app.py
@@ -3,7 +3,7 @@ import numpy as np
 
 st.title("조 추첨 페이지")
 st.header("여러분의 참여를 환영합니다.")
-print("dd")
+str.write("dd")
 # colums = st.colums(4)
 # for idx, c in colums:
 #     st.text_input("조 추첨 대상" key=idx)
