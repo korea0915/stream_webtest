9d13b7b1f0199697a7d25188a781cbe688380290
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 20:14:07 +0900

    zcv

--- opendata/app.py
+++ opendata/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+temp1 = ""
 temp2 = ""
 AI26year = [2018,2019,2020]
 AI26location = ['강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
@@ -15,17 +15,17 @@ with st.sidebar:
         "원하시는 년도를 선택해 주세요",
         (AI26year[0], AI26year[1], AI26year[2])
     )
-if st.sidebar.button('지역 선택'):
-    global temp1 = ""
-    location = st.sidebar.selectbox(
-    "지역 선택",
-        (
-            '강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
-            '강서구', '양천구', '마포구', '서대문구', '은평구', '노원구', '도봉구', '강북구', '성북구',
-            '중랑구', '동대문구', '광진구', '성동구', '용산구', '중구', '종로구'
-        )
-    )
-    temp1 = location
+st.sidebar.button('지역 선택')
+    # if :
+    # location = st.sidebar.selectbox(
+    # "지역 선택",
+    #     (
+    #         '강동구', '송파구', '강남구', '서초구', '관악구', '동작구', '영등포구', '금천구', '구로구',
+    #         '강서구', '양천구', '마포구', '서대문구', '은평구', '노원구', '도봉구', '강북구', '성북구',
+    #         '중랑구', '동대문구', '광진구', '성동구', '용산구', '중구', '종로구'
+    #     )
+    # )
+    # temp1 = location
 st.write(temp1)
 if st.sidebar.button('크기 선택'):
     size = st.sidebar.selectbox(
