fd4f18fccd20d0b57218ef66b62b2db10f9e354c
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 12:10:16 +0900

    글 수정

--- first_webapp/app.py
+++ first_webapp/app.py
@@ -1,13 +1,16 @@
+##streamlit 라이브러리 불러오기
 import streamlit as st
 
+##메인에 글 작성
 st.write(
     """
     # stramlit기반의 웹 페이지연습
     ## 마크다운으로 작성
-    * 테스트 클립니다.
+    * 테스트 입니다.
     """
 )
 
+##이미지 표시
 st.image(
     "https://imagescdn.gettyimagesbank.com/500/201607/a10518606.jpg"
 )
