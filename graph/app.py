c60f08840da7c2b358f3d5a29e2f9eb08e6e66b2
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-13 15:24:57 +0900

    123v

--- graph/app.py
+++ graph/app.py
@@ -1,10 +1,11 @@
 import streamlit as st 
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 titanic = sns.load_dataset("titanic")
 
 st.write(titanic)
 st.write(titanic.info())
+fig = plt.figure(figsize=(10,4))
