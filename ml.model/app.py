cb31f4d4c63495a53049c886f46b5346ad283eef
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-15 17:16:40 +0900

    push

--- ml.model/app.py
+++ ml.model/app.py
@@ -6,3 +6,8 @@ import seaborn as sns
 
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
+
+import joblib
+model = joblib.load(./ml.model/app.py)
+model_info = pd.Series(model.coef_, index =  index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"])
+st.write(model_info)
