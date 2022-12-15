b4c3a982eb2afa5e0371b62dd28c59f413df48cf
korea0915 <31436454+korea0915@users.noreply.github.com>
2022-12-15 17:46:14 +0900

    push1

--- ml.model/app.py
+++ ml.model/app.py
@@ -66,7 +66,6 @@ if st.button("예측"):
         state["sex"] == "남성",
         state["region"] == "북동",
         state["region"] == "북서",
-        state["region"] == "남동",
-        state["region"] == "남서"]]
+        state["region"] == "남동"]]        
     pred = model.predict(input_values)
     st.metric(label="예측값", value= pred[0])
