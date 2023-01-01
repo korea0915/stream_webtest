14b2e74c2ebd09a8d757c68ad4549b0a951cb627
ygkim <nadabtbs@gmail.com>
2023-01-01 15:17:57 +0900

    test

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -6,9 +6,9 @@ import seaborn as sns
 import os
 import openai
 
-openai.organization = "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"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+# openai.organization = "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"
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt="Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nReach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
