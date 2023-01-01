a32a893310284fa198fede8f7223b9bfee88c8c6
ygkim <nadabtbs@gmail.com>
2023-01-01 17:27:28 +0900

    test

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -7,17 +7,15 @@ import openai
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def chat_api(prompt):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
-    temperature=0.5,
-    max_tokens=60,
-    top_p=1.0,
-    frequency_penalty=0.5,
-    presence_penalty=0.0,
-    stop=["You:"]
-  )
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+  temperature=0.5,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.5,
+  presence_penalty=0.0,
+  stop=["You:"]
 
   
 prompt = st.text_input("You: ", "hello, how are you?", key="input")
