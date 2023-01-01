484053ca613f9fd405ec4dd9307db2a324b0d164
ygkim <nadabtbs@gmail.com>
2023-01-01 17:43:10 +0900

    test

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -10,9 +10,10 @@ def chat_api(yinput):
   response = openai.Completion.create(
   model="text-davinci-003",
   prompt=yinput,
+  temperature=0,
   max_tokens=100,
-  top_p=1,
-  frequency_penalty=0.0,
+  top_p=1.0,
+  frequency_penalty=0.2,
   presence_penalty=0.0,
   stop=["\n"]
   )
