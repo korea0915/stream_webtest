3d89893ba0a57fae3f8a178e186c6f936f9536b6
ygkim <nadabtbs@gmail.com>
2023-01-01 16:03:22 +0900

    testetst

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -4,7 +4,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-from streamlit_chat import message
-
-message("My message") 
-message("Hello bot!", is_user=True)  # align's the message to the right
+  
