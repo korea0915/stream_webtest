214170bfe209c5090bf4dd31574766d36d362126
ygkim <nadabtbs@gmail.com>
2023-01-01 14:37:24 +0900

    asd

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import os
 import openai
 
-openai.api_key = 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
+openai.api_key = "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"
 
 response = openai.Completion.create(
   model="text-davinci-003",
