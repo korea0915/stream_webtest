2081695041b932a91782ce6723a42c51803900a2
korea0915 <31436454+korea0915@users.noreply.github.com>
2023-01-02 11:55:26 +0900

    openai #

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -5,23 +5,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import openai
 ##open chatGPT api
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-def chat_api(yinput):
-  response = openai.Completion.create(
-  model="text-curie-001",
-  prompt=yinput,
-  temperature=0.8,
-  max_tokens=60,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-  )
-  message = response.choices[0].text
-  return message
+# openai.api_key = st.secrets["OPENAI_API_KEY"]
+# def chat_api(yinput):
+#   response = openai.Completion.create(
+#   model="text-curie-001",
+#   prompt=yinput,
+#   temperature=0.8,
+#   max_tokens=60,
+#   top_p=1.0,
+#   frequency_penalty=0.0,
+#   presence_penalty=0.0
+#   )
+#   message = response.choices[0].text
+#   return message
     
-yinput = st.text_input("You: ", "hello, how are you?", key="input")
+# yinput = st.text_input("You: ", "hello, how are you?", key="input")
 
-st.write(chat_api(yinput))
+# st.write(chat_api(yinput))
 
 ###코드 시작
 df = pd.read_csv("./mini_ai_project/data/train.csv")
