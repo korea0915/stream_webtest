d15fc9a091b65ca82650e29fb03adae12a19aeb9
korea0915 <31436454+korea0915@users.noreply.github.com>
2023-01-02 11:50:36 +0900

    test

--- mini_ai_project/app.py
+++ mini_ai_project/app.py
@@ -24,5 +24,5 @@ yinput = st.text_input("You: ", "hello, how are you?", key="input")
 st.write(chat_api(yinput))
 
 ###코드 시작
-# df = pd.read_parquet("korea0915/stream_webtest/mini_ai_project/data/train.parquet")
-# st.wirte(df.head())
+df = pd.read_csv("korea0915/stream_webtest/mini_ai_project/data/train.csv")
+st.wirte(df.head())
